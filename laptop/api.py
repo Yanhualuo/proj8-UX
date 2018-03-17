cdc29c4f5833a1c7c83b858e226b453874258f9d
Yanhua <bfone12@gmail.com>
2018-03-16 23:54:02 -0700

    update

--- laptop/api.py
+++ laptop/api.py
@@ -26,7 +26,7 @@ api = Api(app)
 app.config['SECRET_KEY'] = 'Thisisasecret!'
 csrf.init_app(app)
 
-client = MongoClient()
+client = MongoClient('db', 27017)
 db = client.time
 mongo = client.user
 
@@ -100,6 +100,11 @@ def verify_auth_token(token):
 
 
 #############################################
+@app.route('/api/resource')
+@login_required
+def get_resource():
+    return render_template('index.php')
+
 
 class ListAll(Resource):
     def get(self):
@@ -273,7 +278,7 @@ api.add_resource(ListCloseOnlyCsv, '/listCloseOnly/csv')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return redirect(url_for('login'))
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -285,15 +290,15 @@ def login():
         user = users.find_one({"username": form.username.data})
         print(user)
         if user:
-            print("~~~check password: ", user['password'])
+            #print("~~~check password: ", user['password'])
             if check_password_hash(user['password'], form.password.data):
-                #print("hello world")
+                
                 user_obj = User(user['username'])
                 
                 login_user(user_obj, remember=form.remember.data)
                 print("login !!!!!!!!!!!!!!!!!!!!!!!!!!")
                 return redirect(url_for("dashboard"))
-    #flash("Wrong username or password", category='error')
+    
     return render_template('login.html', title='login', form=form)
 
 
@@ -304,12 +309,12 @@ def signup():
     users = mongo.db.users
     existing_user = users.find_one({'username' : form.username.data})
     if form.validate_on_submit():
-        #return '<h1>The username is {}. The password is {}.'.format(form.username.data, form.password.data)
+        
         if existing_user is None:
             hashed_pw = generate_password_hash(form.password.data, method='sha256')
             users.insert({'username': form.username.data, 'password':hashed_pw})
             print(" hash pw is " + hashed_pw)
-            return '<h1>new user create</h1>'
+            return '<h1>new user created, go to login page</h1>'
         else:
             return '<h1> username taken</h1>'
     return render_template('signup.html', form=form)
@@ -325,10 +330,9 @@ def dashboard():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('login'))
 
 
 
-# Run the application
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
